53ab5993fbcd1921633f3bf1197d53702d0c9171
Mark <github@markedmondson.me>
2024-10-23 11:39:01 +0200

    configure behind reverse proxy

--- computer-use-demo/image/entrypoint.sh
+++ computer-use-demo/image/entrypoint.sh
@@ -8,6 +8,9 @@ python http_server.py > /tmp/server_logs.txt 2>&1 &
 
 STREAMLIT_SERVER_PORT=8501 python -m streamlit run computer_use_demo/streamlit.py > /tmp/streamlit_stdout.log &
 
+# Start Nginx (reverse proxy)
+service nginx start
+
 echo "✨ Computer Use Demo is ready!"
 echo "➡️  Open http://localhost:8080 in your browser to begin"
 
